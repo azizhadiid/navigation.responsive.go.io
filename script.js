d12150fa9a22f86df53b50fb8b70ec2bfc31981d
azizhadiid <azizalhadiid88@gmail.com>
2024-02-06 19:37:32 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.nav-mobile')
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.nav-mobile')
+    sidebar.style.display = 'none';
+}
